dfd9182d5c17884cf6e956ba12b8f0ac9d4c63d6
L1nkStrYk3r <244092467+L1nkStrYk3r@users.noreply.github.com>
2025-11-14 16:42:13 -0700

    Initial Commit

---
+++ src/main/java/net/l1nkstryk3r/mq/L1nksMasterQuest.java
@@ -0,0 +1,15 @@
+package net.l1nkstryk3r.mq;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class L1nksMasterQuest implements ModInitializer {
+	public static final String MOD_ID = "l1nks-master-quest";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/l1nkstryk3r/mq/L1nksMasterQuestDataGenerator.java
@@ -0,0 +1,11 @@
+package net.l1nkstryk3r.mq;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class L1nksMasterQuestDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
