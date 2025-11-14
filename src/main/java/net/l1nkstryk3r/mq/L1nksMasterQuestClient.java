c2b44bd89d7e9737860ce651f1da58d36c8a35f3
L1nkStrYk3r <244092467+L1nkStrYk3r@users.noreply.github.com>
2025-11-14 16:54:10 -0700

    Additional setup tweaks

---
+++ src/main/java/net/l1nkstryk3r/mq/L1nksMasterQuestClient.java
@@ -0,0 +1,10 @@
+package net.l1nkstryk3r.mq;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class L1nksMasterQuestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
